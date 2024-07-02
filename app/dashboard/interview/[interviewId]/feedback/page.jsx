69d1e7b275eb1e8cd0e27f0db11501c55b9de158
TianMeds <cbmedallada@student.apc.edu.ph>
2024-07-02 16:31:22 +0800

    Added the feedback screen of the page and also getting the value of the feedback in the Drizzle database

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+'use client'
+import { UserAnswer } from '@/utils/schema'
+import React, { useEffect } from 'react'
+import { db } from '@/utils/db'
+import { eq } from 'drizzle-orm'
 
-const Feedback = () => {
+const Feedback = ({params}) => {
+
+  useEffect(() => {
+    GetFeedback();
+  }, [])
+
+  const GetFeedback=async()=>{
+     const result =  await db.select()
+     .from(UserAnswer)
+     .where(eq(UserAnswer.mockIdRef,params.interviewId))
+     .orderBy(UserAnswer.id);
+
+     console.log(result)
+  }
   return (
-    <div>Feedback</div>
+    <div className='p-10'>
+      <h2 className='text-3xl font-bold text-green-500'>Congratulations!</h2>
+      <h2 className='text-2xl font-bold'>Here is your interview Feedback</h2>
+
+      <h2 className='text-blue-700 text-lg my-3'>Your overall interview rating: <strong>7/10</strong></h2>
+
+      <h2 className='text-sm text-gray-500'>Find below interview question with correct answer, Your answer and feedback for improvement</h2>
+    </div>
   )
 }
 
