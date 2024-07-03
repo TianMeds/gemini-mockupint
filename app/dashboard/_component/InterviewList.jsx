8a8a4e80f8ba9aee1201a415ee341b284b3ac7ba
TianMeds <cbmedallada@student.apc.edu.ph>
2024-07-03 10:02:41 +0800

    Display Interview List in the dashboard

---
+++ app/dashboard/_component/InterviewList.jsx
@@ -0,0 +1,40 @@
+'use client'
+import { db } from '@/utils/db';
+import { MockPreparation } from '@/utils/schema';
+import { useUser } from '@clerk/nextjs'
+import { desc, eq } from 'drizzle-orm';
+import React, { useEffect, useState } from 'react'
+import InterviewCard from './InterviewCard';
+
+const InterviewList = () => {
+
+    const  {user} = useUser();
+    const [interviewList, setInterviewList] = useState([]);
+
+    useEffect(() => {
+        user&&GetInterviewList();
+    }, [user])
+
+    const GetInterviewList = async() => {
+        const result = await db.select()
+        .from(MockPreparation)
+        .where(eq(MockPreparation.createdBy,user?.primaryEmailAddress?.emailAddress))
+        .orderBy(desc(MockPreparation.id))
+
+        console.log(result);
+        setInterviewList(result);
+    }
+  return (
+    <div>
+        <h2 className='font-medium text-xl'>Previous Mock Interview</h2>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-3'>
+            {interviewList&&interviewList.map((interview, index) => (
+                <InterviewCard key={index} interview={interview}/>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default InterviewList
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,17 +1,22 @@
 import { UserButton } from '@clerk/nextjs'
 import AddInterview from './_component/AddInterview'
 import React from 'react'
+import InterviewList from './_component/InterviewList'
 
 const DashboardPage= () => {
   return (
     <div>
       
-    <h2 className='font-bold text-2xl'>Dashboard</h2>
-    <h2 className='text-gray-500'>Create and Start your Mock Up Interview</h2>
+      <h2 className='font-bold text-2xl'>Dashboard</h2>
+      <h2 className='text-gray-500'>Create and Start your Mock Up Interview</h2>
 
-    <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
-      <AddInterview/>
-    </div>
+      <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
+        <AddInterview/>
+      </div>
+
+      {/* Interview List */}
+
+      <InterviewList/>
 
     </div>
 
