372c05c23811e0ca48652b1267d2a2b5b771626a
Christian Medallada <cbmedallada@student.apc.edu.ph>
2024-07-05 22:03:47 +0800

    Fix the AnswerSection of the DB

--- app/dashboard/_component/AnswerSection.jsx
+++ app/dashboard/_component/AnswerSection.jsx
@@ -10,6 +10,8 @@ import { chatSession } from '@/utils/GeminiModel'
 import { UserAnswer } from '@/utils/schema'
 import { useUser } from '@clerk/nextjs'
 import WebcamImage from '../../../public/Webcam.svg'
+import { db } from '@/utils/db'
+import moment from 'moment'
 
 const AnswerSection = ({interviewQuestion, questionIndex, interviewData }) => {
 
@@ -39,43 +41,41 @@ const AnswerSection = ({interviewQuestion, questionIndex, interviewData }) => {
         if(!isRecording&&userAnswer.length>10) {
             UpdateUserAnswer();
         }
-        if(userAnswer?.length<10){
-            setLoading(false)
-            toast('Error while saving your answer. Please record again.')
-            return;
-        }
+        // if(userAnswer?.length<10){
+        //     setLoading(false)
+        //     toast('Error while saving your answer. Please record again.')
+        //     return;
+        // }
       }, [userAnswer])
 
 
       const SaveUserAnswer = async() => {
         if(isRecording) {
             stopSpeechToText();
+            
         }
         else{
             startSpeechToText();
         }
       }
 
-      const UpdateUserAnswer = async()=>{
-
+      const UpdateUserAnswer = async () => {
             console.log(userAnswer)
-
             setLoading(true)
             const feedbackPrompt = "Question: "+ interviewQuestion[questionIndex].question+ ", User Answer:"+ userAnswer + ", Depends on question and user answer for give interview question" + " please give us rating for answer and feedback as area of improvement if any " + "in just 3 to 5 lines to improve it in JSON format with rating  field and feedback field";
 
             const result = await chatSession.sendMessage(feedbackPrompt);
-
+            
             const mockJsonResp=(result.response.text()).replace('```json', '').replace('```', '');
 
-            console.log(mockJsonResp)
-
+            console.log(mockJsonResp);
             const JsonFeedbackResp = JSON.parse(mockJsonResp);
 
             const resp = await db.insert(UserAnswer)
             .values({
                 mockIdRef:interviewData?.mockId,
-                question: interviewQuestion[questionIndex].question,
-                answer: interviewQuestion[questionIndex].answer,
+                question: interviewQuestion[questionIndex]?.question,
+                correctAns: interviewQuestion[questionIndex]?.answer,
                 userAns: userAnswer,
                 feedback: JsonFeedbackResp?.feedback,
                 rating:  JsonFeedbackResp?.rating,
@@ -89,11 +89,12 @@ const AnswerSection = ({interviewQuestion, questionIndex, interviewData }) => {
                 setResults([]);
             }
             setResults([]);
-            setLoading(false);
+            setLoading(false)
       }
+
   return (
     <div className='flex items-center justify-center flex-col'>
-        <div className='flex flex-col mt-20 justify-center items-center bg-black rounded-lg p-5'>
+        <div className='flex flex-col mt-20 justify-center items-center bg-white w-4/5 rounded-lg p-5'>
             <Image src={WebcamImage} width={200} height={200} className='absolute' alt='Camera Icon'/>
             <Webcam
                 mirrored={true}
@@ -104,18 +105,15 @@ const AnswerSection = ({interviewQuestion, questionIndex, interviewData }) => {
                 }}
             />
         </div>
-        <Button
-        disabled={loading}
-        variant="outline" className="my-10"
-        onClick={SaveUserAnswer}
-        >
+        <Button disabled={loading} variant="outline"className="bg-white text-black my-10" onClick={SaveUserAnswer}>
             {isRecording ? 
-                <h2 className='text-red-600 flex gap-2'>
-                    <Mic/> Stop Recording
-                </h2>    
+                <h2 className='text-red flex gap-2'>
+                    <Mic/> Stop Recording   
+                </h2>
 
                 :
-                'Record Answer'
+
+                "Record Answer"
             }
         </Button>
     </div>
